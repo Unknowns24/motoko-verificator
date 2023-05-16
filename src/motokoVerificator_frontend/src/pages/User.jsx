9188772e54b47c672776022e40f048c338110c56
Unknowns24 <unknowns0074@gmail.com>
2023-05-16 14:38:19 -0300

    Redirect to / if registered

--- src/motokoVerificator_frontend/src/pages/User.jsx
+++ src/motokoVerificator_frontend/src/pages/User.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "@hooks/useAuth";
 
 import Loading from "@components/Loading";
@@ -6,12 +7,17 @@ import Loading from "@components/Loading";
 const User = () => {
 	const { backendActor } = useAuth();
 	const [show, setShow] = useState(false);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const verifyRegistered = async () => {
 			const isRegistered = await backendActor.imRegistered();
 
-			setShow(isRegistered);
+			if (isRegistered) {
+				navigate("/");
+			} else {
+				setShow(isRegistered);
+			}
 		};
 
 		verifyRegistered();
